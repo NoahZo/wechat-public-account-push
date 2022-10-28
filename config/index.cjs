243f942ce857ba5f25fb8b287229428eacd1397b
NoahZo <84487875+NoahZo@users.noreply.github.com>
2022-10-28 18:31:57 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'XOeYCtaPzZb6qCMD8xYXCdw21q4CJTJKbGGklhFb_0w',
+  CALLBACK_TEMPLATE_ID: '	B-6CzfCC9oxkTgBlQytfimoetDN9uRVMhM0VGythGpM',
 
   CALLBACK_USERS: [
     {
